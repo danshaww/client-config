8650032f83c966ff2197d9dfdc47137aee17c63f
dan <danshaw509@gmail.com>
2025-01-23 09:18:17 +0000

    Updated script

--- setup.sh
+++ setup.sh
@@ -2,18 +2,29 @@
 
 ### Ensure vars.yml is populated manually before running this script (excluded from git)
 
+# Vars
+repo="https://github.com/epichouse/ansible-roles"
+
 ### Ansible installation
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt update
 sudo apt install ansible -y
 
+# Ansible roles clone/pull step.
 cd ..
-git clone https://github.com/epichouse/ansible-roles
+if [ -d "$repo" ]; then
+        cd $repo && git pull && cd ..
+    else
+        git clone $repourl
+    fi
+    ); done
 cd ansible-dev
 
+# Check for required vars file
 if [ ! -f vars.yml ]; then
     echo "vars.yml not found. Please duplicate vars_template.yml and populate vars before running."
     exit
 fi
 
+# Execute Ansible Playbook
 ansible-playbook playbooks/setup.yml --ask-become-pass
