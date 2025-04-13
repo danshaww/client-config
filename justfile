857832c9ff99aaf79cc4919906e42a73c17e2d87
Dan <danshaw509@gmail.com>
2025-04-13 10:18:48 +0100

    Finished client config changes

--- justfile
+++ justfile
@@ -3,15 +3,15 @@ default:
 
 # Run Full Provisioning
 [no-cd]
-full:
-	/home/$(whoami)/git/epichouse/client-config/scripts/setup-wsl.sh && /home/$(whoami)/git/epichouse/client-config/scripts/setup-git.sh
+run:
+	/home/$(whoami)/git/client-config/scripts/setup-wsl.sh && /home/$(whoami)/git/client-config/scripts/repositories.sh
 
 # Run WSL provisioning script
 [no-cd]
 wsl:
-	/home/$(whoami)/git/epichouse/client-config/scripts/setup-wsl.sh
+	/home/$(whoami)/git/client-config/scripts/setup-wsl.sh
 
 # Run Git provisioning script
 [no-cd]
 git:
-	/home/$(whoami)/git/epichouse/client-config/scripts/setup-git.sh
+	/home/$(whoami)/git/client-config/scripts/repositories.sh
--- scripts/repositories.sh
+++ scripts/repositories.sh
@@ -5,7 +5,26 @@ git_directory=~/git
 git_repos=(
     git@gitea:dan/ansible-core
     git@gitea:dan/ansible-roles
+    git@gitea:dan/ansible-vault
+    git@gitea:dan/ansible-dev
+    git@gitea:dan/terraform-modules
+    git@gitea:dan/terraform-deployment
+    git@gitea:dan/client-config
+    git@gitea:dan/software-licensing
+    git@gitea:dan/code-snippets
+    git@gitea:dan/bash-scripts
+    git@gitea:dan/powershell-scripts
+    git@gitea:dan/python-dev
+    git@gitea:dan/docker-dev
+    git@gitea:dan/homelab-mgmt
 )
+
+
+blue="\033[0;34m"
+white="\033[0m"
+
+# Welcome Message
+printf "${blue}\nWelcome to Dan's Git Repo Cloning Utility\n\nAll repositories listed in the script will be cloned/updated\n\n${white}"
 
 # Change into base git directory
 cd $git_directory
@@ -14,10 +33,12 @@ cd $git_directory
 for repourl in ${git_repos[@]}; do
 (
     repo=$(basename "$repourl" .git)
-    echo $repo
     if [ -d "$repo" ]; then
-        cd $repo && git pull && cd ..
+        echo "Pulling changes for $repo >> $(cd "$repo" && git pull && cd $git_directory)"
     else
         git clone $repourl
     fi
     ); done
+
+# Exit Message
+printf "${blue}\nScript Complete\n\n${white}"
