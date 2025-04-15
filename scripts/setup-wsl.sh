06ecfb20a6ee35bcdb95d66326c97fe2d8433f1e
Dan <danshaw509@gmail.com>
2025-04-15 20:57:28 +0100

    Migrated wsl_base role into this repo

--- scripts/setup-wsl.sh
+++ scripts/setup-wsl.sh
@@ -2,10 +2,6 @@
 
 ### Ensure vars.yml is populated manually before running this script (excluded from git)
 
-# Vars
-repourl="https://github.com/danshaww/ansible-roles"
-repo=$(basename "$repourl" .git)
-
 git pull
 
 # Check for required vars file
@@ -20,14 +16,6 @@ if ! which ansible > /dev/null; then
     sudo apt-add-repository ppa:ansible/ansible
     sudo apt update
     sudo apt install ansible -y
-fi
-
-# Ansible roles clone/pull step.
-cd ..
-if [ -d "$repo" ]; then
-    cd $repo && git pull && cd ../client-config
-else
-    git clone $repourl && cd ../client-config
 fi
 
 # Execute Ansible Playbook
